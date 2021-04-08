dff097ed03ffc9f470e94d95cfcd56233787e1ca
Kenny Choe <Kenneth.Choe@abrigo.com>
2021-04-08 15:57:34 -0500

    default dotvvm app added

---
+++ web-dotvvm/src/DotvvmApp/ViewModels/MasterPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+
+namespace DotvvmApp.ViewModels
+{
+    public class MasterPageViewModel : DotvvmViewModelBase
+    {
+    }
+}
