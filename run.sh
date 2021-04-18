728fa5044d98b462d3997b9a0d16a50694beba82
Kenneth Choe <kenneth.choe@gmail.com>
2021-04-18 07:21:47 -0500

    now it also works on Windows

--- run.sh
+++ run.sh
@@ -2,5 +2,8 @@
 . ./sql/run.sh
 . ./build.sh
 export DOCKER_HOST_IP_ADDR=$(ip addr show | grep "\binet\b.*\bdocker0\b" | awk '{print $2}' | cut -d '/' -f 1)
-docker-compose up -d
-echo "Try http://localhost:8082 and http://localhost:8084"
+if [[ -z $DOCKER_HOST_IP_ADDR ]]; then
+    export DOCKER_HOST_IP_ADDR=host.docker.internal
+fi
+
+docker-compose up -d && echo "Try http://localhost:8082 and http://localhost:8084"
