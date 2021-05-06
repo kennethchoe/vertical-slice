331e5ffa5756abb05ba61cd745a1c92c0013e346
Kenneth Choe <kenneth.choe@gmail.com>
2021-05-05 23:07:16 -0500

    change sql port so it works on linux

--- set-DOCKER_HOST_IP_ADDR.sh
+++ set-DOCKER_HOST_IP_ADDR.sh
@@ -2,8 +2,5 @@
 # when you run docker on Windows WSL, you get host.docker.internal but you don't get a network dedicated to docker.
 # when you run docker on Linux, you don't get host.docker.internal but you can get host ip via ip addr from the didicated network to docker.
 # this script exports DOCKER_HOST_IP_ADDR to indicate docker host name for either case.
-export DOCKER_HOST_IP_ADDR=$(ip addr show | grep "\binet\b.*\bdocker0\b" | awk '{print $2}' | cut -d '/' -f 1)
-if [[ -z $DOCKER_HOST_IP_ADDR ]]; then
-    export DOCKER_HOST_IP_ADDR=$(ifconfig -a | grep inet.*broadcast | awk '{print $2}')
-fi
+export DOCKER_HOST_IP_ADDR=$(hostname -I | awk '{print $1}')
 export DOCKER_HOST_NAME="localhost"
--- sql/run.sh
+++ sql/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DockerSqlPort=41403
+DockerSqlPort=51403
 DockerSaPassword="StrongP@ssw0rd"
 
 export existingContainerId="$(docker ps -aq --filter label=vertical-slice-sql)"
